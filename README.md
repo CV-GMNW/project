3ffc0a215ab2bb3522dafacedaf135ba74ef808a
Gábor Siffel <hugabor@users.noreply.github.com>
2018-11-19 02:48:45 -0500

    created img-to-vid convert

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # project
+
+
+## Usages
+
+vid2img.py usage:
+
+    python vid2img.py <video_path> <output_dir>
+
+img2vid.py usage:
+
+    python img2vid.py <frame_images_dir> <fps INT> <output_video_path>
--- vid_utils/img2vid.py
+++ vid_utils/img2vid.py
@@ -1,1 +1,41 @@
-﻿# TODO
+﻿import cv2
+import sys
+import os
+import numpy as np
+from video import *
+
+def create_video_from_frames(frames, output_path, fps):
+    if len(frames) < 1:
+        return None
+
+    (w, h, _) = np.shape(frames[0])
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video = cv2.VideoWriter(output_path, fourcc, fps, (w, h))
+
+    for image in frames:
+        video.write(image)
+
+    cv2.destroyAllWindows()
+    video.release()
+
+    return load_video(output_path)
+
+def load_frames_from_dir(frames_dir, ext=None):
+    for f in os.listdir(frames_dir):
+        if ext is None or f.endswith('.' + ext):
+            yield cv2.imread(f)
+
+
+
+if __name__ == "__main__":
+
+    frames_dir = sys.argv[1]
+    fps = int(sys.argv[2])
+    video_output_path = sys.argv[3]
+
+    frames = load_frames_from_dir(frames_dir)
+    create_video_from_frames(frames, video_output_path, fps)
+
+    print("Successfully stitched %i frames into a video" % len(frames))
+    print("Saved to file: \"" + video_output_path + "\"")
+
