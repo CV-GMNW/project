a9c70b45f7457216c08ef2cb3a174adc612bba14
Gábor Siffel <hugabor@users.noreply.github.com>
2018-11-19 02:28:59 -0500

    updated video handling

--- vid_utils/vid2img.py
+++ vid_utils/vid2img.py
@@ -1,37 +1,38 @@
 ﻿import cv2
 import sys
 import os
+from video import *
 
-video_path = sys.argv[1]
-output_dir = sys.argv[2]
+def save_frames(frames, output_dir):
+    # create output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-# Load video
-vid = cv2.VideoCapture(video_path)
-
-width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
-fps = vid.get(cv2.CAP_PROP_FPS)
-num_frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
-
-print("Video properties:")
-print("    width: %ipx" % width)
-print("    height: %ipx" % height)
-print("    fps: %i" % fps)
-print("    length: %i frames (%.2f seconds)" % (num_frames, num_frames / fps))
-
-# create output directory if it doesn't exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-
-count = 0
-success = True
-while success:
-    success, image = vid.read()
-    if success:
+    count = 0
+    for image in frames:
         cv2.imwrite(output_dir + ("/frame%06i.jpg" % count), image)
         count += 1
-        if count % fps == 0:
-            print("Read %i seconds of video.." % (count / fps))
 
-print("Successfully created images from %i frames" % count)
-print("Saved to directory: \"" + output_dir + "\"")
+def print_video_characteristics(video):
+    print("Video properties:")
+    print("    width: %ipx" % video.size()[0])
+    print("    height: %ipx" % video.size()[1])
+    print("    fps: %i" % video.fps())
+    print("    length: %i frames (%.2f seconds)" % (video.num_frames(), video.length()))
+
+
+
+if __name__ == "__main__":
+
+    video_path = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    video = load_video(video_path)
+
+    print_video_characteristics(video)
+
+    save_frames(video.frames(), output_dir)
+
+    print("Successfully created images from %i frames" % video.num_frames())
+    print("Saved to directory: \"" + output_dir + "\"")
+
