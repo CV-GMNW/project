4e9a69908200e7ef06ae5189ddcdefad7bf920c4
mloo3 <slayer71432@gmail.com>
2018-11-30 22:15:57 -0500

    some sabilize fix

--- stitch1.py
+++ stitch1.py
@@ -103,7 +103,74 @@ def stitch_frames_METHOD_1(w_orig, h_orig, frames, point_correspondences):
     for i in range(len(frames)):
         (pos_x, pos_y) = frame_positions[i]
         yield place_on_black(frames[i], w_new, h_new, pos_x, pos_y)
+def test(frames):
+    grays= []
+    for f in frames:
+        grays.append(cv2.cvtColor(f, cv2.COLOR_BGR2GRAY))
+    transformation_matrix=np.matrix([[1, 0, 0], [0, 1, 0]])
+    old_img = grays[0]
+    cumulative_x_shift=0
+    cumulative_y_shift=0
+    inter_frame_delay=1
+    rows,cols = frames[0].shape[0],frames[0].shape[1]
+    for gray in range(len(grays)):
+        new_img = grays[gray]
+        sift = cv2.xfeatures2d.SIFT_create(nfeatures=1000,nOctaveLayers=3,edgeThreshold=100,sigma=1)
 
+        kp1, des1 = sift.detectAndCompute(old_img, None)
+        kp2, des2 = sift.detectAndCompute(new_img, None)
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+
+        search_params = dict(checks=50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        matches = flann.knnMatch(des1, des2, k=2)
+        matchesMask = []
+        good = []
+
+        new_matches = []
+        for i in range(len(matches)):
+            if len(matches[i]) == 2:
+              matchesMask.append([0,0])
+              new_matches.append(matches[i])
+
+        for i,m in enumerate(new_matches):
+            if m[0].distance < 0.7 *m[1].distance:
+                  matchesMask[i]=[1,0]
+                  good.append(m[0])
+
+
+        old = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        new = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+        last_transformation_matrix = transformation_matrix
+        transformation_matrix = cv2.estimateRigidTransform(new, old, False)
+
+
+        if transformation_matrix is None:
+            transformation_matrix=last_transformation_matrix
+        x_shift=transformation_matrix[0,2]
+        y_shift=transformation_matrix[1,2]
+
+
+        cumulative_x_shift += x_shift
+        cumulative_y_shift += y_shift
+
+        x_total = cumulative_x_shift
+        y_total = cumulative_y_shift
+
+        correction_transformation=np.matrix([[1, 0, -x_total], [0, 1, -y_total]])
+
+
+        stabilized_frame = cv2.warpAffine(frames[gray],correction_transformation,(cols+1000,rows+1000),flags=cv2.INTER_NEAREST|cv2.WARP_INVERSE_MAP)
+
+        cv2.imshow('original frame',frames[gray])
+        cv2.imshow('stabilised frame',stabilized_frame)
+        cv2.waitKey(inter_frame_delay)
+
+        old_img = new_img
 if __name__ == '__main__':
     vid = load_video('vid_utils/pano_shaky_3sec_small.mp4')
 
@@ -114,12 +181,13 @@ if __name__ == '__main__':
     frames = list(vid.frames())
     print "finding corresponding points between frames..."
     # corr = get_correspondences(frames)
-    corr = get_correspondences2(frames, dist=3, meth='sift')
+    # corr = get_correspondences2(frames, dist=3, meth='sift')
     print "stitching..."
-    new_frames = stitch_frames_METHOD_1(vid.size()[0], vid.size()[1], frames, corr)
-    new_frames_list = [new_frame for new_frame in new_frames]
+    test(frames)
+    # new_frames = stitch_frames_METHOD_1(vid.size()[0], vid.size()[1], frames, corr)
+    # new_frames_list = [new_frame for new_frame in new_frames]
 
     # save_frames(new_frames_list, 'output')
-    create_video_from_frames(new_frames_list, 'stitch1_output.avi', OUTPUT_SIZE[0], OUTPUT_SIZE[1], vid.fps())
+    # create_video_from_frames(new_frames_list, 'stitch1_output.avi', OUTPUT_SIZE[0], OUTPUT_SIZE[1], vid.fps())
     print "Done."
 
