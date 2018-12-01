371b80a31dc499f40d08ec8a215716183e3e8281
Gábor Siffel <hugabor@users.noreply.github.com>
2018-11-30 22:35:56 -0500

    repo cleaning 1

--- img2vid.py
+++ img2vid.py
@@ -10,7 +10,9 @@ def load_frames_from_dir(frames_dir, ext=None):
         if ext is None or f.endswith(ext):
             yield cv2.imread(os.path.join(frames_dir, f))
 
-def create_video_from_frames(frames, output_path, w, h, fps):
+def create_video_from_frames(frames, output_path, fps):
+    h, w, _ = np.shape(frames[0])
+
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
     writer = cv2.VideoWriter(output_path, fourcc, fps, (w, h))
     for image in frames:
--- stitch1.py
+++ stitch1.py
@@ -5,117 +5,65 @@ from video import *
 from vid2img import *
 from img2vid import *
 
-def find_rel_locations(point_correspondences):
-    rel_locs = [[0, 0]]
+# def find_rel_locations(point_correspondences):
+#     rel_locs = [[0, 0]]
 
-    # rel_locs = [[[0], [0]]]
-    #
-    # for i in range(len(frames) - 1):
-    #     for j in range(1, len(frames)):
-    #         if j-i >= 1 and j-i <= 5:
-
-
-    for cli in range(len(point_correspondences)):
-        avg_offset = [0, 0]
-        n = len(point_correspondences[cli])
-        for c in point_correspondences[cli]:
-            x_offs = c[1][0] - c[0][0]
-            y_offs = c[1][1] - c[0][1]
-            avg_offset[0] += float(x_offs) / n
-            avg_offset[1] += float(y_offs) / n
-        rel_locs.append(avg_offset)
-
-    return rel_locs
-
-def mean_without_outliers(data, m=2.0):
-    data = np.array(data)
-    return np.mean(data[abs(data - np.mean(data)) < m * np.std(data)])
-
-def find_locations(corrs_per_frame_2):
-    locs = []
-
-    for corrs in corrs_per_frame_2:
-        averaged_loc = [0., 0.]
-        num = len(corrs)
-        # print num
-        for j in range(len(corrs)):
-            x_offsets = []
-            y_offsets = []
-            for c in corrs[j]:
-                x_offsets.append(c[1][0] - c[0][0])
-                y_offsets.append(c[1][1] - c[0][1])
-            avg_x_offs = mean_without_outliers(x_offsets)
-            avg_y_offs = mean_without_outliers(y_offsets)
-            averaged_loc[0] += float(locs[-1-j][0] + avg_x_offs) / float(num)
-            averaged_loc[1] += float(locs[-1-j][1] + avg_y_offs) / float(num)
-        locs.append(averaged_loc)
-
-    return np.int32(np.array(locs))
-
-def place_on_black(frame, w, h, x, y):
-    stitched_frame = np.zeros((h, w, 3))
-    orig_w = np.shape(frame)[1]
-    orig_h = np.shape(frame)[0]
-    stitched_frame[y:y+orig_h, x:x+orig_w] = frame
-    return stitched_frame
-
-OUTPUT_SIZE = (0, 0)
-
-def stitch_frames_METHOD_1(w_orig, h_orig, frames, point_correspondences):
-    global OUTPUT_SIZE
-
-    # 1. find frames locations relative to original
-    print "  finding locations..."
-    # rel_locs = find_rel_locations(point_correspondences)
-    # cum_rel_locs = [rel_locs[0]]
-    # for loc in rel_locs[1:]:
-    #     cum_sum = [a + b for [a,b] in zip(cum_rel_locs[-1], loc)]
-    #     cum_rel_locs.append(cum_sum)
-
-    # cum_rel_locs = np.int32(np.array(cum_rel_locs))
-    # cum_rel_locs = find_locations(point_correspondences)
-    cum_rel_locs = np.int64(list(test(frames)))
-    # print cum_rel_locs
-
-    # 2. find size of frame needed
-    min_x = 0
-    max_x = 0
-    min_y = 0
-    max_y = 0
-    for rel_loc in cum_rel_locs:
-        if rel_loc[0] < min_x:
-            min_x = rel_loc[0]
-        if rel_loc[0] > max_x:
-            max_x = rel_loc[0]
-        if rel_loc[1] < min_y:
-            min_y = rel_loc[1]
-        if rel_loc[1] > max_y:
-            max_y = rel_loc[1]
-    w_new = max_x + 1 - min_x + w_orig
-    h_new = max_y + 1 - min_y + h_orig
-
-    OUTPUT_SIZE = (w_new, h_new)
-    print "  generating new frames..."
-
-    # 3. position all frames in the new larger frame
-    frame_positions = [(x - min_x, y - min_y) for (x, y) in cum_rel_locs]
-
-    # 4. place frames
-    for i in range(len(frames)):
-        (pos_x, pos_y) = frame_positions[i]
-        yield place_on_black(frames[i], w_new, h_new, pos_x, pos_y)
+#     # rel_locs = [[[0], [0]]]
+#     #
+#     # for i in range(len(frames) - 1):
+#     #     for j in range(1, len(frames)):
+#     #         if j-i >= 1 and j-i <= 5:
 
 
-def test(frames):
+#     for cli in range(len(point_correspondences)):
+#         avg_offset = [0, 0]
+#         n = len(point_correspondences[cli])
+#         for c in point_correspondences[cli]:
+#             x_offs = c[1][0] - c[0][0]
+#             y_offs = c[1][1] - c[0][1]
+#             avg_offset[0] += float(x_offs) / n
+#             avg_offset[1] += float(y_offs) / n
+#         rel_locs.append(avg_offset)
+
+#     return rel_locs
+
+# def mean_without_outliers(data, m=2.0):
+#     data = np.array(data)
+#     return np.mean(data[abs(data - np.mean(data)) < m * np.std(data)])
+
+# def find_locations(corrs_per_frame_2):
+#     locs = []
+
+#     for corrs in corrs_per_frame_2:
+#         averaged_loc = [0., 0.]
+#         num = len(corrs)
+#         # print num
+#         for j in range(len(corrs)):
+#             x_offsets = []
+#             y_offsets = []
+#             for c in corrs[j]:
+#                 x_offsets.append(c[1][0] - c[0][0])
+#                 y_offsets.append(c[1][1] - c[0][1])
+#             avg_x_offs = mean_without_outliers(x_offsets)
+#             avg_y_offs = mean_without_outliers(y_offsets)
+#             averaged_loc[0] += float(locs[-1-j][0] + avg_x_offs) / float(num)
+#             averaged_loc[1] += float(locs[-1-j][1] + avg_y_offs) / float(num)
+#         locs.append(averaged_loc)
+
+#     return np.int32(np.array(locs))
+
+
+
+def find_locations_2(frames):
     grays= []
     for f in frames:
         grays.append(cv2.cvtColor(f, cv2.COLOR_BGR2GRAY))
+
     transformation_matrix=np.matrix([[1, 0, 0], [0, 1, 0]])
+
     old_img = grays[0]
     cumulative_x_shift=0
     cumulative_y_shift=0
-    inter_frame_delay=1
-    rows,cols = frames[0].shape[0],frames[0].shape[1]
     for gray in range(len(grays)):
         new_img = grays[gray]
         sift = cv2.xfeatures2d.SIFT_create(nfeatures=1000,nOctaveLayers=3,edgeThreshold=100,sigma=1)
@@ -151,12 +99,10 @@ def test(frames):
         last_transformation_matrix = transformation_matrix
         transformation_matrix = cv2.estimateRigidTransform(new, old, False)
 
-
         if transformation_matrix is None:
             transformation_matrix=last_transformation_matrix
         x_shift=transformation_matrix[0,2]
         y_shift=transformation_matrix[1,2]
-
 
         cumulative_x_shift += x_shift
         cumulative_y_shift += y_shift
@@ -166,16 +112,60 @@ def test(frames):
 
         # correction_transformation=np.matrix([[1, 0, -x_total], [0, 1, -y_total]])
 
-
         # stabilized_frame = cv2.warpAffine(frames[gray],correction_transformation,(cols+1000,rows+1000),flags=cv2.INTER_NEAREST|cv2.WARP_INVERSE_MAP)
 
         # cv2.imshow('original frame',frames[gray])
         # cv2.imshow('stabilised frame',stabilized_frame)
-        # cv2.waitKey(inter_frame_delay)
+        # cv2.waitKey(1)
 
         yield [x_total, y_total]
 
         old_img = new_img
+
+
+def place_on_black(frame, w, h, x, y):
+    stitched_frame = np.zeros((h, w, 3))
+    orig_w = np.shape(frame)[1]
+    orig_h = np.shape(frame)[0]
+    stitched_frame[y:y+orig_h, x:x+orig_w] = frame
+    return stitched_frame
+
+
+def stitch_frames_METHOD_1(w_orig, h_orig, frames, point_correspondences):
+    global OUTPUT_SIZE
+
+    # 1. find frames locations relative to original
+    print "  finding locations..."
+    relative_locations = np.int64(list(find_locations_2(frames)))
+
+    # 2. find size of frame needed
+    min_x = 0
+    max_x = 0
+    min_y = 0
+    max_y = 0
+    for rel_loc in relative_locations:
+        if rel_loc[0] < min_x:
+            min_x = rel_loc[0]
+        if rel_loc[0] > max_x:
+            max_x = rel_loc[0]
+        if rel_loc[1] < min_y:
+            min_y = rel_loc[1]
+        if rel_loc[1] > max_y:
+            max_y = rel_loc[1]
+    w_new = max_x + 1 - min_x + w_orig
+    h_new = max_y + 1 - min_y + h_orig
+
+    OUTPUT_SIZE = (w_new, h_new)
+    print "  generating new frames..."
+
+    # 3. position all frames in the new larger frame
+    frame_positions = [(x - min_x, y - min_y) for (x, y) in relative_locations]
+
+    # 4. place frames
+    for i in range(len(frames)):
+        (pos_x, pos_y) = frame_positions[i]
+        yield place_on_black(frames[i], w_new, h_new, pos_x, pos_y)
+
 
 
 if __name__ == '__main__':
@@ -186,15 +176,15 @@ if __name__ == '__main__':
 
     print "loading frames..."
     frames = list(vid.frames())
-    print "finding corresponding points between frames..."
-    # corr = get_correspondences(frames)
-    # corr = get_correspondences2(frames, dist=3, meth='sift')
-    print "stitching..."
-    # test(frames)
-    new_frames = stitch_frames_METHOD_1(vid.size()[0], vid.size()[1], frames, None)
-    new_frames_list = [new_frame for new_frame in new_frames]
 
-    # save_frames(new_frames_list, 'output')
-    create_video_from_frames(new_frames_list, 'stitch1_output.avi', OUTPUT_SIZE[0], OUTPUT_SIZE[1], vid.fps())
+    print "finding corresponding points between frames..."
+
+    print "stitching..."
+    new_frames = list(stitch_frames_METHOD_1(vid.size()[0], vid.size()[1], frames, None))
+
+    print "creating video.."
+    create_video_from_frames(new_frames, 'stitch1_output.avi', vid.fps())
+
     print "Done."
+
 
